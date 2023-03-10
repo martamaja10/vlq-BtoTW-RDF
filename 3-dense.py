6594fc0ea491e75303ccd753785c2bfdd9f38952
kyh57363 <kyle-howey@bethel.edu>
2023-03-09 18:31:27 -0600

    Fixed file naming in 3-dense and added MLP script

--- 3-dense.py
+++ 3-dense.py
@@ -23,21 +23,31 @@ np.random.seed(seed)
 
 treename = 'Events'
 filename = {}
+weights = {}
 upfile = {}
 params = {}
 df = {}
 
 print('Performing initializations...')
-# These two lists make things easier later
-filenames = ['ttbarT', 'ttbarTb', #'singleT', 'singleTb', 
-             'WJets2500','WJets1200', 'WJets800', 'WJets600', 'WJets400', 'WJets200',
-             'BpM2000', 'BpM1400', 'BpM800']
 
-weights = [1, 1, #0.456, 0.0506, 
-           0.0011, 0.0148, 0.0544, 0.1128, 0.4749, 0.4466, 1, 1, 1]
+#These two dictionaries make things easier
+
+weights['ttbarT'] = 1
+weights['ttbarTb'] = 1
+#weights['singleT'] = 0.456
+#weights['singleTb'] = 0.0506
+weights['WJets2500'] = 0.0011
+weights['WJets1200'] = 0.0148
+weights['WJets800'] = 0.0544
+weights['WJets600'] = 0.1128
+weights['WJets400'] = 0.4749
+weights['WJets200'] = 0.4466
+weights['BpM2000'] = 1
+weights['BpM1400'] = 1
+weights['BpM800'] = 1
 
 filename['BpM2000'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/BpM2000_hadd.root'
-filename['BpM1400'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/BpM1400_hadd.root'
+filename['BpM1400'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/BpM14000_hadd.root'
 filename['BpM800'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/BpM800_hadd.root'
 filename['WJets1200'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/WJets1200_hadd.root'
 filename['WJets2500'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/WJets2500_hadd.root'
@@ -45,8 +55,8 @@ filename['WJets800'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/WJ
 filename['WJets600'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/WJets600_hadd.root'
 filename['WJets400'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/WJets400_hadd.root'
 filename['WJets200'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/WJets200_hadd.root'
-filename['ttbarT'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/ttbarInc_hadd.root'
-filename['ttbarTb'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/ttbarInc_hadd.root'
+filename['ttbarT'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/ttbarT_hadd.root'
+filename['ttbarTb'] = 'root://cmseos.fnal.gov//store/user/jmanagan/BtoTW_RDF/ttbarTb_hadd.root'
 
 VARS = ['pNet_J_1',#'pNet_J_2',
         'pNet_T_1',#'pNet_T_2',
@@ -61,15 +71,15 @@ VARS = ['pNet_J_1',#'pNet_J_2',
         'NJets_central', 'NJets_DeepFlavM','NFatJets','NJets_forward',
         'Bprime_DR','Bprime_ptbal','Bprime_chi2'] # choose which vars to use (2d)
 
-for i,key in enumerate(filenames): ## Need .keys() here, or different list
+for i,key in enumerate(filename.keys()): ## Need .keys() here, or different list
     print('Now processing file ' + key + '...')
     upfile[key] = uproot.open(filename[key])
     params[key] = upfile[key][treename].arrays(VARS)
-
     df[key] = pd.DataFrame(params[key],columns=VARS)
     df[key] = df[key].loc[(df[key]['Bprime_mass'] > 0) & (df[key]['NJets_forward'] > 1)] # Selection criteria
 
-    df[key]['Weight'] = weights[i]
+    df[key]['Weight'] = weights[key]
+
 
 
 ## cut out undefined variables VARS[0] and VARS[1] > -999
@@ -82,9 +92,10 @@ for i,key in enumerate(filenames): ## Need .keys() here, or different list
         df[key]['isSignal'] = np.ones(len(df[key]))
     else:
         df[key]['isSignal'] = np.zeros(len(df[key]))
+    print(df[key].shape)
+    print(df[key]['Weight'])
 
 print('Completed loading files, pickling input data...')
 df_all = pd.concat([df['BpM2000'], df['BpM1400'], df['BpM800'], df['ttbarT'],df['ttbarTb'], df['WJets200'], 
                     df['WJets400'], df['WJets600'], df['WJets800'], df['WJets1200'], df['WJets2500']])
-np.savez('NewAnalysisArrays', df_all.values)
-
+np.savez('NewAnalysisArrays', dfarray = df_all.values)
