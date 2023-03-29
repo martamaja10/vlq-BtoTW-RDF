102bb5d811d9f77a6c3e0b159a7677cf505b8ab8
xshen <xiaohe_shen@brown.edu>
2023-03-29 10:24:34 -0500

    added QCD and genBranches

--- plotting/histoDrawTrees.py
+++ plotting/histoDrawTrees.py
@@ -8,13 +8,20 @@ from ROOT import *
 iPlot = sys.argv[1] 
 
 # Samples to process and categories to plot
-indir = "/store/user/jmanagan/BtoTW_RDF_MLPs_hadds"
+indir = "/store/user/sxiaohe/vlq-BtoTW-RDF/cut_update1/Bprime_hadds/"
 outdir = os.getcwd()+'/plots_forwardjet/'
 if not os.path.exists(outdir): os.system('mkdir -p '+outdir)
-samples = {'Bp800':'BpM800_hadd.root', 
-           'Bp1400':'BpM1400_hadd.root',
-           'Bp2000':'BpM2000_hadd.root',
-           'ttbar':'ttbarInc_hadd.root',
+samples = {'Bp800':'Bprime800_hadd.root', 
+           'Bp1400':'Bprime1400_hadd.root',
+           'Bp2000':'Bprime2000_hadd.root',
+           #'qcd200':'QCD200_hadd.root',                                  
+           'qcd300':'QCD300_hadd.root',                                  
+           'qcd500':'QCD500_hadd.root',                                  
+           'qcd700':'QCD700_hadd.root',                                  
+           'qcd1000':'QCD1000_hadd.root',                                
+           'qcd1500':'QCD1500_hadd.root',                                
+           'qcd2000':'QCD2000_hadd.root',
+           'ttbar':'ttbar_hadd.root',
            'wjets200':'WJets200_hadd.root',
            'wjets400':'WJets400_hadd.root',
            'wjets600':'WJets600_hadd.root',
@@ -23,13 +30,17 @@ samples = {'Bp800':'BpM800_hadd.root',
            'wjets2500':'WJets2500_hadd.root',
            'singleT':'singleT_hadd.root',
            'singleTb':'singleTb_hadd.root',
-           'data_obs':'ttbarInc_hadd.root' # data is a copy of ttbar for now, need histograms in the file for limits
+           'data_obs':'ttbar_hadd.root' # data is a copy of ttbar for now, need histograms in the file for limits
 }
 tags = {'tjet':'taggedTjet == 1',
         'Wjet':'taggedWjet == 1',
         'Wbjet':'taggedWbjetJet == 1',
         'other':'isValidBDecay < 1', # later likely some other selection like a network score
         'all':'isValidBDecay == 1', # combine all tagged events
+        'trueT':'trueLeptonicMode == 1',
+        'trueW':'trueLeptonicMode == 0',
+        'correctJet':'(taggedTjet == 1 && trueLeptonicMode == 1) || (taggedWjet == 1 && trueLeptonicMode == 0)',
+        'flippedJet':'(taggedTjet == 1 && trueLeptonicMode == 0) || (taggedWjet == 1 && trueLeptonicMode == 1)',
     }
 
 # Numbers to weight the samples relative to each other
@@ -37,8 +48,14 @@ lumi = 138000.0
 nRun = {'Bp800':99800.,
         'Bp1400':99600.,
         'Bp2000':99600.,  # fixme, need a sum-of-gen-weights calculated...
+        #'qcd200':61542214.,
+        'qcd300':56214199.,
+        'qcd500':61097673.,
+        'qcd700':47314826.,
+        'qcd1000':15230975.,
+        'qcd1500':11887406.,
+        'qcd2000':5710430.,
         'ttbar':476408000.,
-        #'wjets':81051269,
         'wjets200':58225632.,
         'wjets400':7444030.,
         'wjets600':7718765.,
@@ -49,8 +66,14 @@ nRun = {'Bp800':99800.,
         'singleTb':95627000.,
         'data_obs':1}
 xsec = {'Bp800':1.0,'Bp1400':1.0,'Bp2000':1.0, #signals all the same at 1pb for now, predictions vary
+        #'qcd200':1712000,
+        'qcd300':347700,
+        'qcd500':32100,
+        'qcd700':6831,
+        'qcd1000':1207,
+        'qcd1500':119.9,
+        'qcd2000':25.24,
         'ttbar':831.76*0.438,
-        #'wjets':61526.7,
         'wjets200':359.7*1.21,
         'wjets400':48.91*1.21,
         'wjets600':12.05*1.21,
@@ -67,8 +90,11 @@ sig2leg='B (2.0 TeV)'
 sig3leg='B (1.4 TeV)'
 sigScaleFact = 1 # zoom in/out on signal
 print 'Scale factor = ',sigScaleFact
-bkgProcList = ['wjets200','wjets400','wjets600','wjets800','wjets1200','wjets2500','ttbar','singleT','singleTb']
-bkgHistColors = {'ttbar':kAzure+8,
+bkgProcList = [#'qcd200',
+'qcd300','qcd500','qcd700','qcd1000','qcd1500','qcd2000','wjets200','wjets400','wjets600','wjets800','wjets1200','wjets2500','ttbar','singleT','singleTb']
+bkgHistColors = {#'qcd200':kYellow-7,
+'qcd300':kYellow-7,'qcd500':kYellow-7,'qcd700':kYellow-7,'qcd1000':kYellow-7,'qcd1500':kYellow-7,'qcd2000':kYellow-7,
+                 'ttbar':kAzure+8,
                  'wjets200':kMagenta-2,'wjets400':kMagenta-2,'wjets600':kMagenta-2,'wjets800':kMagenta-2,'wjets1200':kMagenta-2,'wjets2500':kMagenta-2,
                  'singleT':kGreen-3,'singleTb':kGreen-3,} #TT
 yLog  = True
@@ -110,6 +136,7 @@ plotList = {#discriminantName:(discriminantLJMETName, binning, xAxisLabel)
     'Wdrlep':('DR_W_lep',linspace(0,5,51).tolist(),';leptonic W, #DeltaR(W,lepton)'),
     'tdrWb':('DR_W_b',linspace(0,6.3,51).tolist(),';leptonic t, #DeltaR(W,b)'),
     'isLepW':('leptonicParticle',linspace(0,2,3).tolist(),';lepton from W'),
+    'WMt':('W_MT',linspace(0,1000,51).tolist(),';M_{T}(W) [GeV]'),
     'minMlj':('minMleppJet',linspace(0,1000,51).tolist(),';min[M(l,jet)] [GeV], 0 b tags'),
     'PtRel':('ptRel_lep_Jet',linspace(0,500,51).tolist(),';p_{T,rel}(l, closest jet) [GeV]'),
     'PtRelAK8':('ptRel_lep_FatJet',linspace(0,500,51).tolist(),';p_{T,rel}(l, closest AK8 jet) [GeV]'),
@@ -131,6 +158,12 @@ plotList = {#discriminantName:(discriminantLJMETName, binning, xAxisLabel)
     'mlp_HT250_Bprime':('mlp_HT250_Bprime',linspace(0,1,51).tolist(),';MLP (HT 250) T score'), # later, these should exist
     'mlp_HT250_TTbar': ('mlp_HT250_TTbar',linspace(0,1,51).tolist(),';MLP (HT 250) t#bar{t} score'),
     'mlp_HT250_WJets': ('mlp_HT250_WJets',linspace(0,1,51).tolist(),';MLP (HT 250) W+jets score'),
+
+    "Bprime_gen_pt":('Bprime_gen_pt',linspace(0,1500,51).tolist(),';B quark p_{T gen} [GeV]'),
+    "Bprime_gen_eta":('Bprime_gen_eta',linspace(-8,8,51).tolist(),';B quark #eta _{gen}'),
+    "Bprime_gen_phi":('Bprime_gen_phi',linspace(-3.5,3.5,51).tolist(),';B quark #phi _{gen}'),
+    "Bprime_gen_mass":('Bprime_gen_mass',linspace(700,2000,51).tolist(),';M_{gen}(B) [GeV]'),
+
 }
 
 # open an output file for the histos
@@ -150,7 +183,9 @@ for sample in samples.keys():
         histo.Sumw2()
         
         weightstr = "(Generator_weight*{}*{}/({}*abs(Generator_weight)))".format(lumi,xsec[sample],nRun[sample])
-        ttree.Draw(plotList[iPlot][0]+' >> '+sample+'_'+tag,weightstr+'*(NJets_forward > 0 && Bprime_mass > 0 && '+tags[tag]+')','GOFF')
+        #print('arg1: ', plotList[iPlot][0]+' >> '+sample+'_'+tag)
+        #print('arg2: ', weightstr+'*(NJets_forward > 0 && Bprime_mass > 0 && '+tags[tag]+')')
+        ttree.Draw(plotList[iPlot][0]+' >> '+sample+'_'+tag,weightstr+'*(NJets_forward > 0 && Bprime_mass > 0 && Bprime_gen_mass!=-999 && '+tags[tag]+')','GOFF')
         histo.SetDirectory(0)
         hists[sample+'_'+tag] = histo
 
@@ -204,7 +239,8 @@ if doPlotting:
         hsig800 = hists['Bp800_'+tag]
         hsig1400 = hists['Bp1400_'+tag]
         hsig2000 = hists['Bp2000_'+tag]
-        if hsig2000.Integral()/bkgHT.Integral() > 0.1: sigScaleFact = 1
+        if bkgHT.Integral() != 0:
+            if hsig2000.Integral()/bkgHT.Integral() > 0.1: sigScaleFact = 1
         hsig800.Scale(sigScaleFact)
         hsig1400.Scale(sigScaleFact)
         hsig2000.Scale(sigScaleFact)
@@ -212,7 +248,7 @@ if doPlotting:
         ############################################################
         ############## Making Plots of e+jets, mu+jets and e/mu+jets 
         ############################################################
-		
+    
         stackbkgHT = THStack("stackbkgHT","")
         # set each background a different color
         for proc in bkgProcList:
@@ -224,7 +260,7 @@ if doPlotting:
         sig1Color= kBlack
         sig2Color= kBlack
         sig3Color= kBlack
-			
+
         hsig800.SetLineColor(sig1Color)
         hsig800.SetFillStyle(0)
         hsig800.SetLineWidth(3)
@@ -247,13 +283,13 @@ if doPlotting:
         gPad.SetBottomMargin(0.12)
         gPad.SetLeftMargin(0.11)
 
-	bkgHT.GetYaxis().CenterTitle()
-	if yLog:
-		gPad.SetLogy()
-                bkgHT.SetMinimum(0.101)
-                bkgHT.SetMaximum(200*bkgHT.GetMaximum())
+        bkgHT.GetYaxis().CenterTitle()
+        if yLog:
+            gPad.SetLogy()
+            bkgHT.SetMinimum(0.101)
+            bkgHT.SetMaximum(200*bkgHT.GetMaximum())
 
-	bkgHT.GetXaxis().SetTitleOffset(1.1)
+        bkgHT.GetXaxis().SetTitleOffset(1.1)
 
         bkgHT.Draw("E2")
         stackbkgHT.Draw("same HIST")
@@ -309,6 +345,5 @@ if doPlotting:
 
 print("--- %s min for hists, %s s for plots ---" % (round(plot_time - start_time, 2)/60, round(time.time() - plot_time, 2)))
     
-
 
 
