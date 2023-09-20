93f2fcec5ec10c64f8a4ab8fc2d7eaafd1a0c195
jmhogan <jmhogan@fnal.gov>
2023-09-20 15:52:27 -0500

    run all JERC variations

--- runRDF.C
+++ runRDF.C
@@ -7,6 +7,7 @@
 #include "generatorInfo.cc"
 #include "utilities.cc"
 #include "W_t_reco.cc"
+#include "scalefactors.cc"
 #include <string>
 #include <vector>
 #include <iostream>
@@ -17,6 +18,22 @@ using namespace std;
 
 void runRDF(string testNum1, string testNum2, string inputFile, string year)
 {
-	rdf t(inputFile, "preselTree_" + testNum1, "finalselTree_" + testNum1, testNum1, testNum2, year); // names get set to class members, should be known w/o passing
-	t.analyzer_RDF(testNum1);
+  rdf t(inputFile, testNum1, testNum2, year); // names get set to class members, should be known w/o passing
+
+  //t.analyzer_RDF(testNum1);
+
+  bool isData = false;
+  if(inputFile.find("Single") != std::string::npos || inputFile.find("EGamma") != std::string::npos) isData = true;
+
+  if(isData) t.analyzer_RDF(testNum1,"Nominal");
+  else{
+    vector<TString> shifts = {"Nominal","JECup","JECdn","JERup","JERdn"};
+    for(size_t i = 0; i < shifts.size(); i++){
+      cout << "\nRunning shift " << shifts[i] << endl;
+
+      t.analyzer_RDF(testNum1,shifts[i]);
+    }
+  }
+
+
 };
