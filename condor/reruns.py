4bcba8e375a81f4fdbccf5b3a476082cd5c8088a
jmhogan <jmhogan@fnal.gov>
2024-07-31 02:21:59 -0500

    more buildout in rerun script

--- condor/reruns.py
+++ condor/reruns.py
@@ -2,12 +2,13 @@ import os,sys,subprocess,fnmatch
 
 #dogrep = True
 resubmit = True
-findfails = False
-jobsrunning = True
-splitjobs = False
+findfails = True
+jobsrunning = False
+splitjobs = True
 dumpbadfile = False
+nsplit = 3
 
-logdir = '/uscms/home/jmanagan/nobackup/BtoTW/rdfjobs_Apr2024_fullRun2/'
+logdir = '/uscms/home/jmanagan/nobackup/BtoTW/rdfjobs_Jul2024_fullRun2/'
 
 def findfiles(path, filtre):
     for root, dirs, files in os.walk(path):
@@ -15,19 +16,9 @@ def findfiles(path, filtre):
             yield os.path.join(root, f)
 
 reruns = [
-    'Bprime_M1200_2017UL/Bprime_M1200_2017UL_RDF_26.out',
-    'DYMHT2002018UL/DYMHT2002018UL_RDF_10.out',
-    'DYMHT2002018UL/DYMHT2002018UL_RDF_9.out',
-    'QCDHT3002016UL/QCDHT3002016UL_RDF_3.out',
-    'QCDHT3002016UL/QCDHT3002016UL_RDF_5.out',
-    'QCDHT3002016UL/QCDHT3002016UL_RDF_7.out',
-    'TTToSemiLeptonic2016UL/TTToSemiLeptonic2016UL_RDF_125.out',
-    'TTToSemiLeptonic2017UL/TTToSemiLeptonic2017UL_RDF_242.out',
-    'TTWl2016UL/TTWl2016UL_RDF_10.out',
-    'TTWq2016UL/TTWq2016UL_RDF_6.out',
-    'WJetsHT8002017UL/WJetsHT8002017UL_RDF_21.out',
-    'WJetsHT8002017UL/WJetsHT8002017UL_RDF_24.out',
-]   
+]
+# last was 7/30 00:47 TTsemi
+
 
 if dumpbadfile:
     for rerun in reruns:
@@ -40,12 +31,13 @@ if dumpbadfile:
     
 ## fill reruns via finding jobs that have no .out', (time, memory)
 if findfails:
-    for file in findfiles('/uscms_data/d3/jmanagan/BtoTW/rdfjobs_Apr2024_fullRun2/', '*.job'):
+    for file in findfiles('/uscms_data/d3/jmanagan/BtoTW/rdfjobs_Jul2024_fullRun2/', '*.job'):
         outname = file.replace('.job','.out').replace('F_','F_RDF_').replace('UL_','UL_RDF_').replace('A_','A_RDF_').replace('B_','B_RDF_').replace('C_','C_RDF_').replace('D_','D_RDF_').replace('E_','E_RDF_').replace('G_','G_RDF_').replace('H_','H_RDF_')
         if not os.path.exists(outname):
             if not jobsrunning: reruns.append(outname[55:])
             else: os.system('ls -l '+file) # use time stamp to remove new running jobs
-            
+
+print(reruns[0:2])
 resubs = {}
 
 if not resubmit: exit()
@@ -55,6 +47,7 @@ for rerun in reruns:
     command = 'grep "Arguments" '+rerun
     proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     (out, err) = proc.communicate()
+    out = out.decode('utf-8')
     jobnums = [int(out.split(" ")[4]),int(out.split(" ")[5])]
     
     print('\''+rerun+'\':'+str(jobnums))
@@ -73,21 +66,42 @@ for rerun in resubs.keys():
             os.system('condor_submit '+jobfile)
             continue
 
-        half = int(resubs[rerun][0] + (resubs[rerun][1] - resubs[rerun][0])/2.0)
-        print(folder+': new ranges '+str(resubs[rerun][0])+' - '+str(half)+' and '+str(half+1)+' - '+str(resubs[rerun][1]))
+        if nsplit == 2: 
+            #half = int(resubs[rerun][0] + (resubs[rerun][1] - resubs[rerun][0])/2.0)
+            newjobfile = jobfile.replace('_'+str(resubs[rerun][0]),'_'+str(half+1))
+            os.chdir(logdir+folder)
+            os.system('cp '+jobfile+' '+newjobfile)
+            os.system('sed -i "s| '+str(resubs[rerun][1])+' | '+str(half)+' |" '+jobfile) # only change upper in original file
+            os.system('sed -i "s| '+str(resubs[rerun][0])+' | '+str(half+1)+' |" '+newjobfile) # only change lower in new file
+            os.system('sed -i "s|_'+str(resubs[rerun][0])+'\.|_'+str(half+1)+'\.|" '+newjobfile)
+            
+            os.system('condor_submit '+jobfile)
+            os.system('condor_submit '+newjobfile) 
+
+        if nsplit == 3:
+            third = int(resubs[rerun][0] + (resubs[rerun][1] - resubs[rerun][0])/3.0)
+            third2 = int(resubs[rerun][0] + 2*(resubs[rerun][1] - resubs[rerun][0])/3.0)
+            print(folder+': new ranges '+str(resubs[rerun][0])+' - '+str(third)+' , '+str(third+1)+' - '+str(third2)+', and '+str(third2+1)+' - '+str(resubs[rerun][1]))
     
-        newjobfile = jobfile.replace('_'+str(resubs[rerun][0]),'_'+str(half+1))
+            newjobfile = jobfile.replace('_'+str(resubs[rerun][0]),'_'+str(third+1))
+            newjobfile2 = jobfile.replace('_'+str(resubs[rerun][0]),'_'+str(third2+1))
         
-        os.chdir(logdir+folder)
-        os.system('cp '+jobfile+' '+newjobfile)
-        os.system('sed -i "s| '+str(resubs[rerun][1])+' | '+str(half)+' |" '+jobfile) # only change upper in original file
-        os.system('sed -i "s| '+str(resubs[rerun][0])+' | '+str(half+1)+' |" '+newjobfile) # only change lower in new file
-        os.system('sed -i "s|_'+str(resubs[rerun][0])+'\.|_'+str(half+1)+'\.|" '+newjobfile)
-        
-        os.system('condor_submit '+jobfile)
-        os.system('condor_submit '+newjobfile) 
+            os.chdir(logdir+folder)
+            os.system('cp '+jobfile+' '+newjobfile)
+            os.system('cp '+jobfile+' '+newjobfile2)
+            os.system('sed -i "s| '+str(resubs[rerun][1])+' | '+str(third)+' |" '+jobfile) # only change upper in original file
+
+            os.system('sed -i "s| '+str(resubs[rerun][1])+' | '+str(third2)+' |" '+newjobfile) # change upper in middle file
+            os.system('sed -i "s| '+str(resubs[rerun][0])+' | '+str(third+1)+' |" '+newjobfile) # change lower in middle file
+            os.system('sed -i "s|_'+str(resubs[rerun][0])+'\.|_'+str(third+1)+'\.|" '+newjobfile)
+
+            os.system('sed -i "s| '+str(resubs[rerun][0])+' | '+str(third2+1)+' |" '+newjobfile2) # only change lower in last file
+            os.system('sed -i "s|_'+str(resubs[rerun][0])+'\.|_'+str(third2+1)+'\.|" '+newjobfile2)
+            
+            os.system('condor_submit '+jobfile)
+            os.system('condor_submit '+newjobfile)
+            os.system('condor_submit '+newjobfile2) 
     else:
         os.chdir(logdir+folder)
         os.system('condor_submit '+jobfile)
             
-
